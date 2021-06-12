9be839699f9d8755acca519fd018bd0d39540bab
sohyun123 <carriesohyun@gmail.com>
2021-06-13 02:09:47 +0900

    refactor : update putLogo function

--- signMaker/addLogo.py
+++ signMaker/addLogo.py
@@ -3,13 +3,17 @@ import os
 
 def putLogo(email, imgPath,str_number):
     image = cv2.imread(imgPath)
-    print(image.shape)
-    x = int(image.shape[1]/11)
-    y = int(image.shape[0]/5)*3
-    imageWithLogo = cv2.putText(image, "The Signature", (x, y), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, fontScale=0.8, thickness=2,color=(220,220,220))
+    overlay = image.copy()
+    x = int(image.shape[1] / 11)
+    y = int(image.shape[0] / 9) * 5
+    imageWithLogo = cv2.putText(overlay, "The Signature", (x, y), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, fontScale=1.1,
+                                thickness=2, color=(179, 174, 174))
+
+    alpha = 0.8  # Transparency factor.
+    image_logo = cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0)
 
     logoImgDir = './signMaker/static/logo_result/' + email + '/'
     if os.path.isdir(logoImgDir)==False:
         os.mkdir(logoImgDir)
 
-    cv2.imwrite(logoImgDir + 'handwriting_name' + str(str_number) + '.png', imageWithLogo)
+    cv2.imwrite(logoImgDir + 'handwriting_name' + str(str_number) + '.png', image_logo)
